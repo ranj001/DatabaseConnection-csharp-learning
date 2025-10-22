d03716da2a9775613661e358422009256972e39c
Lafeguera, Ranjit <Ranjit.Lafeguera@analog.com>
2025-10-22 16:48:48 +0800

    Added DbCommand.cs

---
+++ DatabaseConnection/DbCommand.cs
@@ -0,0 +1,26 @@
+﻿namespace DatabaseConnection
+{
+    public class DbCommand
+    {
+        private DbConnection _connection;
+        public DbCommand(DbConnection connection)
+        {
+            if (connection == null)
+                throw new ArgumentNullException(nameof(connection), "Connection cannot be null.");
+            // Additional initialization code can go here
+            _connection = connection;
+
+        }
+
+        public void Execute(string instruction)
+        {
+            if (string.IsNullOrWhiteSpace(instruction))
+                throw new ArgumentException("Command text cannot be null or empty.", nameof(instruction));
+            // Implementation for executing the command
+
+            _connection.Open();
+            Console.WriteLine($"Executing command: {instruction}");
+            _connection.Close();
+        }
+    }
+}
--- DatabaseConnection/Program.cs
+++ DatabaseConnection/Program.cs
@@ -1,13 +1,24 @@
 ﻿namespace DatabaseConnection
 {
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            var sqlConnection = new SqlConnection("Server=myServer;Database=mySqlDB;User Id=myUser;Password=myPass;");
-            sqlConnection.Open();
-            sqlConnection.Close();
+            //var sqlConnection = new SqlConnection("Server=mySQLServer;Database=mySqlDB;User Id=mySQLUser;Password=mySQLPass;");
+            //sqlConnection.Open();
+            //sqlConnection.Close();
 
+            //var oracleConnection = new OracelConnection("Data Source=myOracleDB;User Id=myOracleUser;Password=myOraclePass;");
+            //oracleConnection.Open();   
+            //oracleConnection.Close();
+
+            var sqlCommand = new DbCommand(new SqlConnection("Server=mySQLServer;Database=mySqlDB;User Id=mySQLUser;Password=mySQLPass;"));
+            sqlCommand.Execute("SELECT * FROM Users");
+
+            var oracleCommand = new DbCommand(new OracelConnection("Data Source=myOracleDB;User Id=myOracleUser;Password=myOraclePass;"));
+            oracleCommand.Execute("SELECT * FROM Employees");
         }
     }
 }
